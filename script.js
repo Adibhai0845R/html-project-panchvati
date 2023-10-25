e71a3dde02ec255e0d7b976a62dcb1b123bb2630
Adibhai0845R <146322971+Adibhai0845R@users.noreply.github.com>
2023-10-26 01:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sale</title>
+    </head>
+    <body>Payment form</body>
+    <script>
+    
+    </script>
+    </html>
